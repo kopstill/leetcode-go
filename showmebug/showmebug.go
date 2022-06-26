09998994d0b81c9c98560917caaf752a6e3db473
kopever <kop.still@gmail.com>
2022-06-26 18:10:23 +0800

    showmebug: bugfix

--- showmebug/showmebug.go
+++ showmebug/showmebug.go
@@ -1,10 +1,13 @@
 package showmebug
 
+type DirType string
+
 const (
-	EAST  = "EAST"
-	WEST  = "WEST"
-	NORTH = "NORTH"
-	SOUTH = "SOUTH"
+	EAST    DirType = "EAST"
+	WEST    DirType = "WEST"
+	NORTH   DirType = "NORTH"
+	SOUTH   DirType = "SOUTH"
+	UNKNOWN DirType = "UNKNOWN"
 )
 
 func dirReduce(directions []string) []string {
@@ -12,39 +15,30 @@ func dirReduce(directions []string) []string {
 		return directions
 	}
 
-	var result []string
-	for i := 0; i < len(directions); i++ {
-		direction := directions[i]
+	for i := 0; i < len(directions)-1; i++ {
+		cuDirection := directions[i]
+		reDirection := getReDirection(cuDirection)
+		neDirection := directions[i+1]
 
-		if i == 0 {
-			nextDirection := directions[i+1]
-			if (direction == EAST && nextDirection != WEST) ||
-				(direction == WEST && nextDirection != EAST) ||
-				(direction == SOUTH && nextDirection != NORTH) ||
-				(direction == NORTH && nextDirection != SOUTH) {
-				result = append(result, direction)
-			}
-			continue
-		}
-
-		if i == len(directions)-1 {
-			preDirection := directions[i-1]
-			if (direction == EAST && preDirection != WEST) ||
-				(direction == WEST && preDirection != EAST) ||
-				(direction == SOUTH && preDirection != NORTH) ||
-				(direction == NORTH && preDirection != SOUTH) {
-				result = append(result, direction)
-			}
-			break
-		}
-
-		if nextDirection, preDirection := directions[i+1], directions[i-1]; (direction == EAST && nextDirection != WEST && preDirection != WEST) ||
-			(direction == WEST && nextDirection != EAST && preDirection != EAST) ||
-			(direction == SOUTH && nextDirection != NORTH && preDirection != NORTH) ||
-			(direction == NORTH && nextDirection != SOUTH && preDirection != SOUTH) {
-			result = append(result, direction)
+		if reDirection == neDirection {
+			return dirReduce(append(directions[:i], directions[i+2:]...))
 		}
 	}
 
-	return result
+	return directions
+}
+
+func getReDirection[T string](direction T) T {
+	switch direction {
+	case T(EAST):
+		return T(WEST)
+	case T(WEST):
+		return T(EAST)
+	case T(SOUTH):
+		return T(NORTH)
+	case T(NORTH):
+		return T(SOUTH)
+	default:
+		return T(UNKNOWN)
+	}
 }
--- showmebug/showmebug_test.go
+++ showmebug/showmebug_test.go
@@ -6,7 +6,12 @@ import (
 )
 
 func TestDirReduce(t *testing.T) {
-	params := []string{"EAST", "SOUTH", "NORTH", "WEST"}
+	// params := []string{"WEST", "SOUTH", "EAST", "NORTH"}
+	// params := []string{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}
+	// params := []string{"NORTH", "SOUTH", "EAST", "WEST"}
+	// params := []string{"NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"}
+	// params := []string{"NORTH", "WEST", "SOUTH", "EAST", "WEST"}
+	params := []string{"NORTH", "WEST", "SOUTH", "EAST", "WEST", "NORTH", "EAST", "XXX", "SOUTH", "WEST", "WEST", "EAST", "EAST", "XXX"}
 	result := dirReduce(params)
 	for _, v := range result {
 		fmt.Println(v)
