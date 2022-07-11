74afd176347a36d450a6917d05154608ea247df4
kopever <kop.still@gmail.com>
2022-07-11 15:33:13 +0800

    problem2: official solution

--- leetcode_test.go
+++ leetcode_test.go
@@ -13,6 +13,48 @@ func TestTwoSum(t *testing.T) {
 }
 
 func TestAddTwoNumbers(t *testing.T) {
+	l1 := ListNode{2, &ListNode{4, &ListNode{3, nil}}}
+	l2 := ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+
+	// l1 := ListNode{4, &ListNode{9, &ListNode{5, nil}}}
+	// l2 := ListNode{3, &ListNode{2, &ListNode{5, nil}}}
+
+	// l1 := ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, nil}}}}}}}
+	// l2 := ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, nil}}}}
+
+	result := addTwoNumbers(&l1, &l2)
+	for {
+		fmt.Println(result.Val)
+		if result.Next != nil {
+			result = result.Next
+		} else {
+			break
+		}
+	}
+}
+
+func TestAddTwoNumbers1(t *testing.T) {
+	// l1 := ListNode{2, &ListNode{4, &ListNode{3, nil}}}
+	// l2 := ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+
+	l1 := ListNode{4, &ListNode{9, &ListNode{5, nil}}}
+	l2 := ListNode{3, &ListNode{2, &ListNode{5, nil}}}
+
+	// l1 := ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, nil}}}}}}}
+	// l2 := ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, nil}}}}
+
+	result := addTwoNumbers1(&l1, &l2)
+	for {
+		fmt.Println(result.Val)
+		if result.Next != nil {
+			result = result.Next
+		} else {
+			break
+		}
+	}
+}
+
+func TestAddTwoNumbers2(t *testing.T) {
 	// l1 := ListNode{2, &ListNode{4, &ListNode{3, nil}}}
 	// l2 := ListNode{5, &ListNode{6, &ListNode{4, nil}}}
 
@@ -22,7 +64,7 @@ func TestAddTwoNumbers(t *testing.T) {
 	l1 := ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, nil}}}}}}}
 	l2 := ListNode{9, &ListNode{9, &ListNode{9, &ListNode{9, nil}}}}
 
-	result := addTwoNumbers1(&l1, &l2)
+	result := addTwoNumbers2(&l1, &l2)
 	for {
 		fmt.Println(result.Val)
 		if result.Next != nil {
