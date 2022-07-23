b6f91b16e0158a4b80428d04ec9f0d0d576dcecc
kopever <kop.still@gmail.com>
2022-07-23 16:43:13 +0800

    problem6: simulation using two-dimensional matrix

--- zigzag_conversion.go
+++ zigzag_conversion.go
@@ -1,20 +1,24 @@
 // 6: https://leetcode.cn/problems/zigzag-conversion/
 package leetcode
 
+import "bytes"
+
+// 此解法不会浪费二维数组的空间
+// 时间复杂度：O(r * n)，二维数组的创建和遍历
+// 空间复杂度：O(r * n)，二维数组占用的空间
 func convert(s string, numRows int) string {
-	if len(s) < 2 || numRows < 2 {
+	n, r := len(s), numRows
+	if r == 1 || r >= n {
 		return s
 	}
 
-	t := make([][]string, numRows)
+	t := make([][]string, r)
 
-	p := 0
-	q := 0
-	for i := 0; i < len(s); i++ {
-		x := append(t[p], string(s[i]))
-		t[p] = x
+	p, q := 0, 0
+	for i := 0; i < n; i++ {
+		t[p] = append(t[p], string(s[i]))
 
-		m := i % (numRows - 1)
+		m := i % (r - 1)
 		q = q % 2
 
 		if i == 0 {
@@ -34,12 +38,85 @@ func convert(s string, numRows int) string {
 		}
 	}
 
-	var r string
+	var res string
 	for _, v := range t {
-		for _, vv := range v {
-			r += vv
+		for _, s := range v {
+			res += s
 		}
 	}
 
-	return r
+	return res
+}
+
+// 基于以上解法创建二维字节矩阵优化时间和空间复杂度（无需手动遍历矩阵）
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+func convert1(s string, numRows int) string {
+	n, r := len(s), numRows
+	if r == 1 || r >= n {
+		return s
+	}
+
+	t := make([][]byte, r)
+
+	p, q := 0, 0
+	for i := 0; i < n; i++ {
+		t[p] = append(t[p], s[i])
+
+		m := i % (r - 1)
+		q = q % 2
+
+		if i == 0 {
+			p++
+		} else {
+			if m == 0 && q == 0 {
+				p--
+				q++
+			} else if m == 0 && q != 0 {
+				p++
+				q--
+			} else if m != 0 && q == 0 {
+				p++
+			} else {
+				p--
+			}
+		}
+	}
+
+	return string(bytes.Join(t, nil))
+}
+
+// 官方解法 1：利用二维矩阵模拟
+// 时间复杂度：O(r⋅n)，其中 r = numRows，n 为字符串 s 的长度。时间主要消耗在矩阵的创建和遍历上，矩阵的行数为 r，列数可以视为 O(n)。
+// 空间复杂度：O(r⋅n)。矩阵需要 O(r⋅n) 的空间。
+func convert2(s string, numRows int) string {
+	n, r := len(s), numRows
+	if r == 1 || r >= n {
+		return s
+	}
+	t := r*2 - 2
+	c := (n + t - 1) / t * (r - 1)
+	mat := make([][]byte, r)
+	for i := range mat {
+		mat[i] = make([]byte, c)
+	}
+	x, y := 0, 0
+	for i, ch := range s {
+		mat[x][y] = byte(ch)
+		if i%t < r-1 {
+			x++ // 向下移动
+		} else {
+			x--
+			y++ // 向右上移动
+		}
+	}
+	ans := make([]byte, 0, n)
+	for _, row := range mat {
+		for _, ch := range row {
+			if ch > 0 {
+				ans = append(ans, ch)
+			}
+		}
+	}
+	return string(ans)
 }
--- zigzag_conversion_test.go
+++ zigzag_conversion_test.go
@@ -14,3 +14,15 @@ func TestZigZagConversion(t *testing.T) {
 	// numRows := 1
 	fmt.Println(convert(s, numRows))
 }
+
+func Test1ZigZagConversion(t *testing.T) {
+	s := "THISISANFIELD"
+	numRows := 3
+	fmt.Println(convert1(s, numRows))
+}
+
+func Test2ZigZagConversion(t *testing.T) {
+	s := "THISMEANSMORE"
+	numRows := 3
+	fmt.Println(convert2(s, numRows))
+}
