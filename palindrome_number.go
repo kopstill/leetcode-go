73f26070963fb7c3081d1a917e9d3b351413a060
kopever <kop.still@gmail.com>
2022-08-23 00:35:04 +0800

    problem9: official solution

--- palindrome_number.go
+++ palindrome_number.go
@@ -1,6 +1,8 @@
 // 9: https://leetcode.cn/problems/palindrome-number/
 package leetcode
 
+// 时间复杂度：O(log(n))，每次迭代将目标数除以 10
+// 空间复杂度：O(1)，无需额外空间存储额外变量
 func isPalindrome(x int) bool {
 	if x < 0 || (x%10 == 0 && x != 0) {
 		return false
@@ -8,9 +10,8 @@ func isPalindrome(x int) bool {
 
 	new := 0
 	for {
-		last := x % 10
 		x = x / 10
-		new = new*10 + last
+		new = new*10 + x%10
 		if new == x {
 			return true
 		}
@@ -23,4 +24,21 @@ func isPalindrome(x int) bool {
 	}
 
 	return false
+}
+
+// 官方解法
+// 时间复杂度：O(log(n))
+// 空间复杂度：O(1)
+func isPalindrome1(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+
+	revertedNumber := 0
+	for revertedNumber < x {
+		revertedNumber = revertedNumber*10 + x%10
+		x /= 10
+	}
+
+	return revertedNumber == x || revertedNumber/10 == x
 }
--- palindrome_number_test.go
+++ palindrome_number_test.go
@@ -12,3 +12,8 @@ func TestIsPalindrome(t *testing.T) {
 	x := 12345054321
 	fmt.Println(isPalindrome(x))
 }
+
+func Test1IsPalindrome(t *testing.T) {
+	x := 6
+	fmt.Println(isPalindrome1(x))
+}
