ac3679507b0282220fea6b7242e499f9f054e3ea
kopever <kop.still@gmail.com>
2022-08-24 17:45:03 +0800

    problem7: official solution

--- reverse_integer.go
+++ reverse_integer.go
@@ -3,7 +3,24 @@ package leetcode
 
 import "math"
 
-func reverse(x int) (rev int) {
+// 时间复杂度：O(log(n))
+// 空间复杂度：O(1)
+func reverse(x int) int {
+	reversedNumber := 0
+	for x > 0 || x < 0 {
+		reversedNumber = reversedNumber*10 + x%10
+		x /= 10
+	}
+	if reversedNumber > math.MaxInt32 || reversedNumber < math.MinInt32 {
+		return 0
+	}
+	return reversedNumber
+}
+
+// 官方解法
+// 时间复杂度：O(log|x|)。翻转的次数即 x 十进制的位数。
+// 空间复杂度：O(1)
+func reverse1(x int) (rev int) {
 	for x != 0 {
 		if rev < math.MinInt32/10 || rev > math.MaxInt32/10 {
 			return 0
--- reverse_integer_test.go
+++ reverse_integer_test.go
@@ -8,6 +8,13 @@ import (
 func TestReverseInteger(t *testing.T) {
 	// x := 123
 	// x := 0
-	x := -123654789
+	x := -12365472345234523
 	fmt.Println(reverse(x))
+}
+
+func Test1ReverseInteger(t *testing.T) {
+	// x := 123
+	// x := 0
+	x := -123456789
+	fmt.Println(reverse1(x))
 }
