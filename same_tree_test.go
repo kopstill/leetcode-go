b3f3908f81094b422e81e5bc23c93ab95f9ba8e9
kopever <kop.still@gmail.com>
2022-09-02 22:30:43 +0800

    problem100: breadth first search

--- same_tree_test.go
+++ same_tree_test.go
@@ -12,3 +12,41 @@ func TestIsSameTree(t *testing.T) {
 	t2 := &TreeNode{1, &TreeNode{2, &TreeNode{4, nil, nil}, &TreeNode{5, nil, nil}}, &TreeNode{3, &TreeNode{6, nil, nil}, &TreeNode{8, nil, nil}}}
 	fmt.Println(isSameTree(t1, t2))
 }
+
+func Test1IsSameTree(t *testing.T) {
+	// t1 := &TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, nil, nil}}
+	// t2 := &TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, nil, nil}}
+	t1 := &TreeNode{1,
+		&TreeNode{2,
+			&TreeNode{4,
+				&TreeNode{8, nil, nil},
+				&TreeNode{9, nil, nil}},
+			&TreeNode{5,
+				&TreeNode{10, nil, nil},
+				&TreeNode{11, nil, nil}}},
+		&TreeNode{3,
+			&TreeNode{6,
+				&TreeNode{12, nil, nil},
+				&TreeNode{13, nil, nil}},
+			&TreeNode{7,
+				&TreeNode{14, nil, nil},
+				&TreeNode{15, nil, nil}}},
+	}
+	t2 := &TreeNode{1,
+		&TreeNode{2,
+			&TreeNode{4,
+				&TreeNode{8, nil, nil},
+				&TreeNode{9, nil, nil}},
+			&TreeNode{5,
+				&TreeNode{10, nil, nil},
+				&TreeNode{11, nil, nil}}},
+		&TreeNode{3,
+			&TreeNode{6,
+				&TreeNode{12, nil, nil},
+				&TreeNode{13, nil, nil}},
+			&TreeNode{7,
+				&TreeNode{14, nil, nil},
+				&TreeNode{15, nil, nil}}},
+	}
+	fmt.Println(isSameTree1(t1, t2))
+}
