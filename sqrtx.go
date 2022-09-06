d16c2128107c127b88e5a5736bf6c116ce63f436
kopever <kop.still@gmail.com>
2022-09-06 17:48:09 +0800

    problem69: binary search algorithm

--- sqrtx.go
+++ sqrtx.go
@@ -35,3 +35,21 @@ func mySqrt1(x int) int {
 	}
 	return ans
 }
+
+// 二分查找
+// 时间复杂度：O(logn)
+// 空间复杂度：O(1)
+func mySqrt2(x int) int {
+	l, r := 0, x
+	ans := -1
+	for l <= r {
+		mid := l + (r-l)/2
+		if mid*mid <= x {
+			ans = mid
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return ans
+}
--- sqrtx_test.go
+++ sqrtx_test.go
@@ -14,3 +14,8 @@ func Test1Sqrt(t *testing.T) {
 	x := 19
 	fmt.Println(mySqrt1(x))
 }
+
+func Test2Sqrt(t *testing.T) {
+	x := 100
+	fmt.Println(mySqrt2(x))
+}
