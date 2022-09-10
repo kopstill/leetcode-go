e23fefcc8d25320d288352980cb35a873ce9b9cb
kopever <kop.still@gmail.com>
2022-09-10 19:31:51 +0800

    problem206: use recursion

--- reverse_linked_list.go
+++ reverse_linked_list.go
@@ -15,3 +15,16 @@ func reverseList(head *ListNode) *ListNode {
 	}
 	return prev
 }
+
+// 递归
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
+func reverseList1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	newHead := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
+}
--- reverse_linked_list_test.go
+++ reverse_linked_list_test.go
@@ -13,3 +13,12 @@ func TestReverseList(t *testing.T) {
 		result = result.Next
 	}
 }
+
+func Test1ReverseList(t *testing.T) {
+	head := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, &ListNode{6, nil}}}}}}
+	result := reverseList1(head)
+	for result != nil {
+		fmt.Println(result.Val)
+		result = result.Next
+	}
+}
