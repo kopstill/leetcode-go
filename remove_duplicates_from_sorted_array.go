e6be37143472e72a588af12d681b037b0e58cce4
kopever <kop.still@gmail.com>
2022-09-10 19:20:46 +0800

    problem26: use recursion

--- remove_duplicates_from_sorted_array.go
+++ remove_duplicates_from_sorted_array.go
@@ -1,20 +1,24 @@
 // 26: https://leetcode.cn/problems/remove-duplicates-from-sorted-array/
 package leetcode
 
-// func removeDuplicates(nums []int) int {
-// 	for i, num := range nums {
-// 		if i < len(nums)-1 && num == nums[i+1] {
-// 			nums = append(nums[:i], nums[i+1:]...)
-// 			removeDuplicates(nums)
-// 		}
-// 	}
+// 递归
+// 时间复杂度：O(n) * O(logn)
+// 空间复杂度：O(logn)
+func removeDuplicates(nums []int) int {
+	for i, num := range nums {
+		if i < len(nums)-1 && num == nums[i+1] {
+			nums = append(nums[:i], nums[i+1:]...)
+			return removeDuplicates(nums)
+		}
+	}
 
-// 	return len(nums)
-// }
+	return len(nums)
+}
 
+// 官方：双指针
 // 时间复杂度：O(n)
 // 空间复杂度：O(1)
-func removeDuplicates(nums []int) int {
+func removeDuplicates1(nums []int) int {
 	n := len(nums)
 	if n == 0 {
 		return 0
--- remove_duplicates_from_sorted_array_test.go
+++ remove_duplicates_from_sorted_array_test.go
@@ -5,9 +5,16 @@ import (
 	"testing"
 )
 
-func TestRemoveDuplicates(t *testing.T) {
+func Test_RemoveDuplicates(t *testing.T) {
 	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	fmt.Println(nums)
 	fmt.Println(removeDuplicates(nums))
+	fmt.Println(nums)
+	fmt.Println(len(nums))
+	fmt.Println(cap(nums))
+}
+
+func Test_1_RemoveDuplicates(t *testing.T) {
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	fmt.Println(removeDuplicates1(nums))
 	fmt.Println(nums)
 }
