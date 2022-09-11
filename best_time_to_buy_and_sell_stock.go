1e831d6dcaba3e0c1fbdc031bfc0531bca701ca2
kopever <kop.still@gmail.com>
2022-09-11 18:57:59 +0800

    problem121: single iteration

--- best_time_to_buy_and_sell_stock.go
+++ best_time_to_buy_and_sell_stock.go
@@ -1,6 +1,8 @@
 // 121: https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
 package leetcode
 
+import "math"
+
 // 暴力循环
 // 时间复杂度：O(n^2)
 // 空间复杂度：O(1)
@@ -19,6 +21,28 @@ func maxProfit(prices []int) int {
 					maxProfit = currProfit
 				}
 			}
+		}
+	}
+
+	return maxProfit
+}
+
+// 一次遍历
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+func maxProfit1(prices []int) int {
+	n := len(prices)
+	if n <= 1 {
+		return 0
+	}
+
+	minPrice := math.MaxInt
+	maxProfit := 0
+	for i := 0; i < n; i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
 		}
 	}
 
--- best_time_to_buy_and_sell_stock_test.go
+++ best_time_to_buy_and_sell_stock_test.go
@@ -9,3 +9,9 @@ func TestMaxProfit(t *testing.T) {
 	prices := []int{10, 2, 30, 4, 2, 1, 100}
 	fmt.Println(maxProfit(prices))
 }
+
+func Test1MaxProfit(t *testing.T) {
+	// prices := []int{10, 5, 6, 1, 20, 8, 30}
+	prices := []int{2, 1, 2, 0, 1}
+	fmt.Println(maxProfit1(prices))
+}
