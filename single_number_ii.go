9730dc8d86814fd75ec77c7927566168d94ab1e9
kopever <kop.still@gmail.com>
2022-09-16 22:49:56 +0800

    problem137: determine each binary bit in turn

--- single_number_ii.go
+++ single_number_ii.go
@@ -7,7 +7,7 @@ package leetcode
 func singleNumberII(nums []int) int {
 	cache := make(map[int]int)
 	for _, num := range nums {
-		cache[num] = cache[num] + 1
+		cache[num]++
 	}
 	for k, v := range cache {
 		if v == 1 {
@@ -15,4 +15,21 @@ func singleNumberII(nums []int) int {
 		}
 	}
 	return -1
+}
+
+// 依次确定每一个二进制位（官方）
+// 时间复杂度：O(nlogC)，其中 n 是数组的长度，C 是元素的数据范围，在本题中 log C = log 2^32 = 32，也就是我们需要遍历第 0∼31 个二进制位。
+// 空间复杂度：O(1)。
+func singleNumberII1(nums []int) int {
+	ans := int32(0)
+	for i := 0; i < 32; i++ {
+		total := int32(0)
+		for _, num := range nums {
+			total += int32(num) >> i & 1
+		}
+		if total%3 > 0 {
+			ans |= 1 << i
+		}
+	}
+	return int(ans)
 }
--- single_number_ii_test.go
+++ single_number_ii_test.go
@@ -9,3 +9,8 @@ func TestSingleNumberII(t *testing.T) {
 	nums := []int{0, 1, 0, 1, 0, 1, 99}
 	fmt.Println(singleNumberII(nums))
 }
+
+func Test1SingleNumberII(t *testing.T) {
+	nums := []int{0, 1, 0, 1, 0, 1, 99, 0, 1}
+	fmt.Println(singleNumberII1(nums))
+}
