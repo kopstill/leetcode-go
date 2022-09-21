e605a7156f67ef88da7d620818d45130582249df
kopever <kop.still@gmail.com>
2022-09-21 21:51:20 +0800

    problem182: without having

--- sql/duplicate_emails.sql
+++ sql/duplicate_emails.sql
@@ -6,3 +6,11 @@
 
 select Email from Person group by Email having count(*) > 1;
 select Email from Person group by Email having count(Email) > 1;
+
+select Email from
+(
+  select Email, count(Email) as num
+  from Person
+  group by Email
+) as statistic
+where num > 1;
