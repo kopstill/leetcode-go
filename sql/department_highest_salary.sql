173e23dca8521d7dc9430157023ad6995f5dbdcf
kopever <kop.still@gmail.com>
2022-09-22 20:37:28 +0800

    problem184: department highest salary

---
+++ sql/department_highest_salary.sql
@@ -0,0 +1,21 @@
+/*
+    184: https://leetcode.cn/problems/department-highest-salary/
+ */
+
+SELECT
+    Department.name AS 'Department',
+    Employee.name AS 'Employee',
+    Salary
+FROM
+    Employee
+        JOIN
+    Department ON Employee.DepartmentId = Department.Id
+WHERE
+    (Employee.DepartmentId , Salary) IN
+    (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+	)
+;
