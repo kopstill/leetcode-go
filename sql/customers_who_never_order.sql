f617f35b9d28114a3cc6a69212d30e83d5b7e572
kopever <kop.still@gmail.com>
2022-09-22 13:24:14 +0800

    problem183: not exists

--- sql/customers_who_never_order.sql
+++ sql/customers_who_never_order.sql
@@ -4,6 +4,8 @@
 
 -- Write your MySQL query statement below
 
+-- not in
+select name Customers from Customers where Id not in (select CustomerId from Orders);
 select c.Name Customers from Customers c where c.Id not in (select o.CustomerId from Orders o);
 
 select customers.name as 'Customers' from customers
@@ -12,7 +14,11 @@ where customers.id not in
     select customerid from orders
 );
 
+-- not exists
+select t1.Name AS Customers from Customers AS t1 
+where not exists (select 1 from Orders AS t2 where t1.id = t2.CustomerId);
+
 -- left join
 select c.Name as Customers from Customers as c
     left join Orders as o on c.Id = o.CustomerId
-where o.Id is null
+where o.Id is null;
