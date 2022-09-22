57d9963b270f5a8c8aef6d2352cdd0a81309eb86
kopever <kop.still@gmail.com>
2022-09-22 13:14:33 +0800

    problem183: official solution

--- sql/customers_who_never_order.sql
+++ sql/customers_who_never_order.sql
@@ -4,4 +4,10 @@
 
 -- Write your MySQL query statement below
 
-select c.Name Customers from Customers c where c.Id not in (select o.CustomerId from Orders o)
+select c.Name Customers from Customers c where c.Id not in (select o.CustomerId from Orders o);
+
+select customers.name as 'Customers' from customers
+where customers.id not in
+(
+    select customerid from orders
+);
