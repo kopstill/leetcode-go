c43da6cfc3675a598f6765df501bc1e3846c1d31
kopever <kop.still@gmail.com>
2022-09-22 20:51:24 +0800

    problem184: use window function

--- sql/department_highest_salary.sql
+++ sql/department_highest_salary.sql
@@ -19,3 +19,14 @@ WHERE
         GROUP BY DepartmentId
 	)
 ;
+
+-- Rank() | Dense_Rank() | Row_Number() 函数， MySQL >= 8.0
+SELECT S.NAME 'Department', S.EMPLOYEE, S.SALARY
+  FROM (SELECT D.NAME,
+               T.NAME EMPLOYEE,
+               T.SALARY,
+               RANK() OVER(PARTITION BY T.DEPARTMENTID ORDER BY T.SALARY DESC) R
+          FROM EMPLOYEE T
+          LEFT JOIN DEPARTMENT D
+            ON T.DEPARTMENTID = D.ID) S
+ WHERE S.R = 1;
