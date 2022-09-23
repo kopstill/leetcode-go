7e686840db5bac37c4c5ad0771754818648e6bfc
kopever <kop.still@gmail.com>
2022-09-23 18:08:10 +0800

    problem511: window function

--- sql/game_play_analysis_i.sql
+++ sql/game_play_analysis_i.sql
@@ -5,3 +5,6 @@
 -- Write your MySQL query statement below
 
 select player_id, min(event_date) as first_login from Activity group by player_id;
+
+-- window function
+select distinct player_id, min(event_date) over(partition by player_id) as first_login from Activity;
