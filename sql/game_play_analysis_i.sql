e9cbf5fc376f28bb7f4af878021327efa0631a66
kopever <kop.still@gmail.com>
2022-09-23 18:06:45 +0800

    problem511: game play analysis i

---
+++ sql/game_play_analysis_i.sql
@@ -0,0 +1,7 @@
+/*
+    511: https://leetcode.cn/problems/game-play-analysis-i/
+*/
+
+-- Write your MySQL query statement below
+
+select player_id, min(event_date) as first_login from Activity group by player_id;
