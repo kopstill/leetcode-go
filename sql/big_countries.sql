6af20b8353f2d4857b40ed545a113ad2d7d7e684
kopever <kop.still@gmail.com>
2022-09-24 22:51:03 +0800

    problem595: union

--- sql/big_countries.sql
+++ sql/big_countries.sql
@@ -5,3 +5,21 @@
 -- Write your MySQL query statement below
 
 select name, population, area from World where area >= 3000000 or population >= 25000000;
+
+-- union
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area >= 3000000
+
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population >= 25000000
+;
