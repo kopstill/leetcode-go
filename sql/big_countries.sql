f39c3aaf01c2d0c1cf445cf62ff56190f7524cd4
kopever <kop.still@gmail.com>
2022-09-24 22:48:57 +0800

    problem595: big countries

---
+++ sql/big_countries.sql
@@ -0,0 +1,7 @@
+/*
+    595: https://leetcode.cn/problems/big-countries/
+ */
+
+-- Write your MySQL query statement below
+
+select name, population, area from World where area >= 3000000 or population >= 25000000;
