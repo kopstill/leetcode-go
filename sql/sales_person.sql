a60ffae599aecce48e0d9f58cc23821152aed71e
kopever <kop.still@gmail.com>
2022-09-26 12:36:15 +0800

    problem607: left (outer) join

--- sql/sales_person.sql
+++ sql/sales_person.sql
@@ -5,9 +5,24 @@
 -- Write your MySQL query statement below
 
 select name from SalesPerson sp 
-    where sales_id not in (
-        select sales_id from Orders o 
+    where sp.sales_id not in (
+        select o.sales_id from Orders o 
             where o.com_id = (
-                select com_id from Company where name = 'RED'
+                select c.com_id from Company c where c.name = 'RED'
             )
     );
+
+SELECT
+    s.name
+FROM
+    salesperson s
+WHERE
+    s.sales_id NOT IN (SELECT
+            o.sales_id
+        FROM
+            orders o
+                LEFT JOIN
+            company c ON o.com_id = c.com_id
+        WHERE
+            c.name = 'RED')
+;
