6397bd6174caeb597e47db485aaf40669e93f736
kopever <kop.still@gmail.com>
2022-09-27 23:10:47 +0800

    problem1084: not between

--- sql/sales-analysis-iii.sql
+++ sql/sales-analysis-iii.sql
@@ -23,3 +23,24 @@ select product_id, product_name from Product
         select product_id from Sales
             where sale_date < '2019-01-01' or sale_date > '2019-03-31'
     );
+
+-- not between
+select distinct a.product_id, b.product_name
+    from sales a, product b
+        where a.product_id = b.product_id
+            and a.product_id not in (
+                select product_id
+                    from sales 
+                        where sale_date 
+                            not between '2019-01-01' and '2019-03-31'
+            );
+
+select distinct a.product_id, b.product_name
+    from sales a inner join product b
+        on a.product_id = b.product_id
+            where a.product_id not in (
+                select product_id
+                    from sales 
+                        where sale_date 
+                            not between '2019-01-01' and '2019-03-31'
+            );
