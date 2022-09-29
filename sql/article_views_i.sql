d237da0390ab0f88109f71faad734786c5bec3ba
kopever <kop.still@gmail.com>
2022-09-29 17:34:10 +0800

    problem1148: group by

--- sql/article_views_i.sql
+++ sql/article_views_i.sql
@@ -4,4 +4,8 @@
 
 -- Write your MySQL query statement below
 
+-- distinct
 select distinct author_id as id from Views where author_id = viewer_id group by author_id order by author_id;
+
+-- group by
+select author_id as id from Views where author_id = viewer_id group by id order by id;
