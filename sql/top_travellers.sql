8b700f9e026e48ec3a747e7e58aaac817768634e
kopever <kop.still@gmail.com>
2022-09-30 22:40:38 +0800

    problem1407: if isnull

--- sql/top_travellers.sql
+++ sql/top_travellers.sql
@@ -4,8 +4,16 @@
 
 -- Write your MySQL query statement below
 
+-- ifnull
 select u.name, ifnull(sum(r.distance), 0) as travelled_distance 
     from Rides r right join Users u 
         on r.user_id = u.id 
     group by r.user_id 
     order by travelled_distance desc, name;
+
+-- if + isnull
+select u.name, if(isnull(r.distance) = 1, 0, sum(r.distance)) as travelled_distance 
+    from Rides r right join Users u 
+        on r.user_id = u.id 
+    group by r.user_id 
+    order by travelled_distance desc, name asc;
