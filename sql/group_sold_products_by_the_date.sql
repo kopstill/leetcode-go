c7f38ea7ad80a7e2550c016c8dd952943f918279
kopever <kop.still@gmail.com>
2022-10-01 14:45:18 +0800

    problem1484: group_concat usage

--- sql/group_sold_products_by_the_date.sql
+++ sql/group_sold_products_by_the_date.sql
@@ -8,6 +8,15 @@
 select sell_date, 
         count(distinct product) num_sold, 
         group_concat(distinct product) products 
-    from Activities 
-    group by sell_date 
-    order by sell_date;
+from Activities 
+group by sell_date 
+order by sell_date;
+
+
+-- group_concat(distinct field order by field desc/asc separator ',')
+SELECT sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product asc SEPARATOR ',') AS products
+FROM Activities 
+GROUP BY sell_date
+ORDER BY sell_date;
