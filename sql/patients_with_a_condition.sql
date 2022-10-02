cd0defb786989ad5c5264c8002ddbe7026806709
kopever <kop.still@gmail.com>
2022-10-02 22:09:36 +0800

    problem1527: patients with a condition

---
+++ sql/patients_with_a_condition.sql
@@ -0,0 +1,8 @@
+/*
+    1527: https://leetcode.cn/problems/patients-with-a-condition/
+ */
+
+-- Write your MySQL query statement below
+
+-- like
+select patient_id, patient_name, conditions from Patients where conditions like 'DIAB1%' or conditions like '% DIAB1%';
