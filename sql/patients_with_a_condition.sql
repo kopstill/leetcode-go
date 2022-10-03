48a70e3bb511360ed3203f78e7789aefa5e95f08
kopever <kop.still@gmail.com>
2022-10-03 15:51:14 +0800

    problem1527: regexp

--- sql/patients_with_a_condition.sql
+++ sql/patients_with_a_condition.sql
@@ -6,3 +6,6 @@
 
 -- like
 select patient_id, patient_name, conditions from Patients where conditions like 'DIAB1%' or conditions like '% DIAB1%';
+
+-- regexp
+SELECT * FROM PATIENTS WHERE CONDITIONS REGEXP '^DIAB1|\\sDIAB1';
