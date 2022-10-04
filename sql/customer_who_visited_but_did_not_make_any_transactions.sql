9b2bde05b1d3c7adfb04c8c6a36caf3aa6736c88
kopever <kop.still@gmail.com>
2022-10-04 10:50:56 +0800

    problem1581: left join

--- sql/customer_who_visited_but_did_not_make_any_transactions.sql
+++ sql/customer_who_visited_but_did_not_make_any_transactions.sql
@@ -4,8 +4,20 @@
 
 -- Write your MySQL query statement below
 
-select customer_id, count(*) as count_no_trans from Visits 
+select 
+    customer_id, count(*) as count_no_trans 
+from 
+    Visits 
 where visit_id not in (
     select visit_id from Transactions
 ) 
-group by customer_id
+group by customer_id;
+
+SELECT 
+    customer_id, COUNT(customer_id) count_no_trans
+FROM 
+    visits v
+LEFT JOIN 
+    transactions t ON v.visit_id = t.visit_id
+WHERE amount IS NULL
+GROUP BY customer_id;
