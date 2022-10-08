a0d6ef0b2ec14f2d618cf6cc5a0996df79f7c7a4
kopever <kop.still@gmail.com>
2022-10-08 20:44:46 +0800

    problem196: without SELECT statement

--- sql/delete_duplicate_emails.sql
+++ sql/delete_duplicate_emails.sql
@@ -6,3 +6,13 @@
 -- Write your MySQL query statement below
 
 delete from Person where id not in (select id from (select min(id) id from Person p group by email) t);
+
+DELETE 
+    p1 
+FROM 
+    Person p1,
+    Person p2
+WHERE
+    p1.Email = p2.Email 
+    AND 
+    p1.Id > p2.Id;
