cd8e17b3b3809d692486422e79ae4441044d7f1a
kopever <kop.still@gmail.com>
2022-10-12 18:00:54 +0800

    problem169: sort and compare

--- majority_element.go
+++ majority_element.go
@@ -1,6 +1,9 @@
 // 169: https://leetcode.cn/problems/majority-element/
 package leetcode
 
+import "sort"
+
+// 使用 hash
 // 时间复杂度：O(n)
 // 空间复杂度：O(n)
 func majorityElement(nums []int) int {
@@ -22,4 +25,27 @@ func majorityElement(nums []int) int {
 	}
 
 	return -1
+}
+
+// 先排序，然后计算个数
+// 时间复杂度：O(n)
+// 空间复杂度：O(1)
+func majorityElement1(nums []int) (val int) {
+	sort.Ints(nums)
+
+	len := len(nums)
+	count := 1
+	val = nums[0]
+	for i := 0; i < len-1; i++ {
+		if nums[i] == nums[i+1] {
+			count++
+		} else {
+			count = 1
+		}
+		if count > len/2 {
+			val = nums[i]
+			break
+		}
+	}
+	return
 }
--- majority_element_test.go
+++ majority_element_test.go
@@ -9,3 +9,9 @@ func TestMajorityElement(t *testing.T) {
 	nums := []int{2, 2, 1, 1, 1, 2, 2}
 	fmt.Println(majorityElement(nums))
 }
+
+func Test1MajorityElement(t *testing.T) {
+	// nums := []int{2, 2, 1, 1, 1, 2, 2}
+	nums := []int{1}
+	fmt.Println(majorityElement1(nums))
+}
