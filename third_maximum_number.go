847514b579c7f03f4c003bbc4d33376141071c2f
kopever <kop.still@gmail.com>
2022-10-18 15:48:01 +0800

    problem414: red black tree

--- third_maximum_number.go
+++ third_maximum_number.go
@@ -3,10 +3,13 @@ package leetcode
 
 import (
 	"sort"
+
+	"github.com/emirpasic/gods/trees/redblacktree"
 )
 
-// 时间复杂度：O(n)
-// 空间复杂度：O(1)
+// 排序
+// 时间复杂度：O(nlogn)，其中 n 是数组 nums 的长度。排序需要 O(nlogn) 的时间。
+// 空间复杂度：O(logn)。排序需要的栈空间为 O(logn)。
 func thirdMax(nums []int) int {
 	sort.Ints(nums)
 
@@ -22,4 +25,35 @@ func thirdMax(nums []int) int {
 	}
 
 	return nums[len-1]
+}
+
+// 排序
+// 时间复杂度：O(nlogn)，其中 n 是数组 nums 的长度。排序需要 O(nlogn) 的时间。
+// 空间复杂度：O(logn)。排序需要的栈空间为 O(logn)。
+func thirdMax1(nums []int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+	for i, diff := 1, 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			diff++
+			if diff == 3 {
+				return nums[i]
+			}
+		}
+	}
+	return nums[0]
+}
+
+// 有序集合
+func thirdMax2(nums []int) int {
+	t := redblacktree.NewWithIntComparator()
+	for _, num := range nums {
+		t.Put(num, nil)
+		if t.Size() > 3 {
+			t.Remove(t.Left().Key)
+		}
+	}
+	if t.Size() == 3 {
+		return t.Left().Key.(int)
+	}
+	return t.Right().Key.(int)
 }
--- third_maximum_number_test.go
+++ third_maximum_number_test.go
@@ -10,3 +10,13 @@ func TestThirdMax(test *testing.T) {
 	nums := []int{1, 2}
 	fmt.Println(thirdMax(nums))
 }
+
+func Test1ThirdMax(test *testing.T) {
+	nums := []int{2, 2, 2, 3, 3, 1, 1, 1, 0}
+	fmt.Println(thirdMax1(nums))
+}
+
+func Test2ThirdMax(test *testing.T) {
+	nums := []int{3, 5, 1}
+	fmt.Println(thirdMax2(nums))
+}
