4bc634352df4588340f54c8b6dc727708cbf15d6
kopever <kop.still@gmail.com>
2022-10-23 20:31:29 +0800

    problem387: array count

--- first_unique_character_in_a_string.go
+++ first_unique_character_in_a_string.go
@@ -1,7 +1,8 @@
 // 387: https://leetcode.cn/problems/first-unique-character-in-a-string/
 package leetcode
 
-func firstUniqChar(s string) int { // leetcodel
+// 暴力轮询
+func firstUniqChar(s string) int {
 	for i := 0; i < len(s); i++ {
 		isUnique := true
 		for j := 0; j < len(s); j++ {
@@ -13,6 +14,22 @@ func firstUniqChar(s string) int { // leetcodel
 			}
 		}
 		if isUnique {
+			return i
+		}
+	}
+
+	return -1
+}
+
+// 数组计数
+func firstUniqChar1(s string) int {
+	count := [26]int{}
+	for i := 0; i < len(s); i++ {
+		count[s[i]-'a']++
+	}
+
+	for i := 0; i < len(s); i++ {
+		if count[s[i]-'a'] == 1 {
 			return i
 		}
 	}
--- first_unique_character_in_a_string_test.go
+++ first_unique_character_in_a_string_test.go
@@ -10,3 +10,11 @@ func TestFirstUniqChar(t *testing.T) {
 	s := "aabb"
 	fmt.Println(firstUniqChar(s))
 }
+
+func Test1FirstUniqChar(t *testing.T) {
+	// s := "leetcode"
+	// s := "aabb"
+	// s := ""
+	s := "xabcx"
+	fmt.Println(firstUniqChar1(s))
+}
