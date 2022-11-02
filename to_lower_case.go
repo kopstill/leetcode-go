3d42e51c92e812afda3068cf03eacfb305b9b4cf
kopever <kop.still@gmail.com>
2022-11-03 00:05:55 +0800

    problem709: rune operation

--- to_lower_case.go
+++ to_lower_case.go
@@ -5,6 +5,9 @@ import (
 	"strings"
 )
 
+// 通过大小写之间的距离实现转换
+// 时间复杂度：O(n)，其中 n 是字符串 s 的长度。
+// 空间复杂度：O(1)，不考虑返回值的空间占用。
 func toLowerCase(s string) string {
 	sb := strings.Builder{}
 	for _, c := range s {
@@ -15,4 +18,25 @@ func toLowerCase(s string) string {
 		}
 	}
 	return sb.String()
+}
+
+// 使用语言内置 API
+// 时间复杂度：O(n)，其中 n 是字符串 s 的长度。
+// 空间复杂度：O(1)，不考虑返回值的空间占用。
+func toLowerCase1(s string) string {
+	return strings.ToLower(s)
+}
+
+// 时间复杂度：O(n)，其中 n 是字符串 s 的长度。
+// 空间复杂度：O(1)，不考虑返回值的空间占用。
+func toLowerCase2(s string) string {
+	lower := &strings.Builder{}
+	lower.Grow(len(s))
+	for _, ch := range s {
+		if 65 <= ch && ch <= 90 {
+			ch |= 32
+		}
+		lower.WriteRune(ch)
+	}
+	return lower.String()
 }
--- to_lower_case_test.go
+++ to_lower_case_test.go
@@ -9,3 +9,13 @@ func TestToLowerCase(t *testing.T) {
 	s := "LeetCode"
 	fmt.Println(toLowerCase(s))
 }
+
+func Test1ToLowerCase(t *testing.T) {
+	s := "Hello World!!!"
+	fmt.Println(toLowerCase1(s))
+}
+
+func Test2ToLowerCase(t *testing.T) {
+	s := "LOVELY"
+	fmt.Println(toLowerCase2(s))
+}
