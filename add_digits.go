3422909477e51ed408725294d7dc78202fa820b5
kopever <kop.still@gmail.com>
2022-11-06 21:29:18 +0800

    problem258: simulation

--- add_digits.go
+++ add_digits.go
@@ -2,6 +2,8 @@
 package leetcode
 
 // 递归
+// 时间复杂度：O(logN)
+// 空间复杂度：O(1)
 func addDigits(num int) int {
 	sum := 0
 	for num > 0 {
@@ -14,4 +16,22 @@ func addDigits(num int) int {
 	}
 
 	return addDigits(sum)
+}
+
+// 模拟
+// 时间复杂度：O(log num)，其中 num 是给定的整数。
+//
+//	对于 num 计算一次各位相加需要 O(log num) 的时间，由于 num ≤ 2(31) − 1，
+//	因此对于 num 计算一次各位相加的最大可能结果是 82，对于任意两位数最多只需要计算两次各位相加的结果即可得到一位数。
+//
+// 空间复杂度：O(1)。
+func addDigits1(num int) int {
+	for num >= 10 {
+		sum := 0
+		for ; num > 0; num /= 10 {
+			sum += num % 10
+		}
+		num = sum
+	}
+	return num
 }
--- add_digits_test.go
+++ add_digits_test.go
@@ -9,3 +9,8 @@ func TestAddDigits(t *testing.T) {
 	num := 12138
 	fmt.Println(addDigits(num))
 }
+
+func Test1AddDigits(t *testing.T) {
+	num := 9527
+	fmt.Println(addDigits1(num))
+}
