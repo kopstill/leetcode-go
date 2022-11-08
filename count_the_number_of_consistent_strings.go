ea4af72497b540a19e4caf52380511d77f103935
kopever <kop.still@gmail.com>
2022-11-08 17:25:27 +0800

    problem1684: bit operation

--- count_the_number_of_consistent_strings.go
+++ count_the_number_of_consistent_strings.go
@@ -3,7 +3,7 @@ package leetcode
 
 import "github.com/emirpasic/gods/sets/hashset"
 
-// set
+// set + 遍历
 func countConsistentStrings(allowed string, words []string) int {
 	count := 0
 	set := hashset.New()
@@ -20,4 +20,24 @@ func countConsistentStrings(allowed string, words []string) int {
 		}
 	}
 	return count
+}
+
+// 位运算 + 遍历
+// 时间复杂度：O(n+∑m_i)，其中 n 是字符串 allowed 的长度，m_i 是字符串 words[i] 的长度。
+// 空间复杂度：O(1)。
+func countConsistentStrings1(allowed string, words []string) (res int) {
+	mask := 0
+	for _, c := range allowed {
+		mask |= 1 << (c - 'a')
+	}
+	for _, word := range words {
+		mask1 := 0
+		for _, c := range word {
+			mask1 |= 1 << (c - 'a')
+		}
+		if mask1|mask == mask {
+			res++
+		}
+	}
+	return
 }
--- count_the_number_of_consistent_strings_test.go
+++ count_the_number_of_consistent_strings_test.go
@@ -10,3 +10,9 @@ func TestCountConsistentStrings(t *testing.T) {
 	words := []string{"cba", "bac", "cab", "abcd", "dbac", "ccc", "aaa", "bbb"}
 	fmt.Println(countConsistentStrings(allowed, words))
 }
+
+func Test1CountConsistentStrings(t *testing.T) {
+	allowed := "test"
+	words := []string{"testestest", "tteesstt", "leetcode", "hello", "world", "set"}
+	fmt.Println(countConsistentStrings1(allowed, words))
+}
