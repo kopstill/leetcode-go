5c8995d021fe56404b9840b7445eda7525dcf41e
kopever <kop.still@gmail.com>
2023-04-24 00:42:55 +0800

    problem217: for loop

--- contains_duplicate.go
+++ contains_duplicate.go
@@ -1,7 +1,9 @@
 // 217: https://leetcode.cn/problems/contains-duplicate/
 package leetcode
 
-// hash
+// Hash
+// Time  complexity: O(n)
+// Space complexity: O(n)
 func containsDuplicate(nums []int) bool {
 	hash := map[int]int{}
 	for i, v := range nums {
@@ -9,6 +11,21 @@ func containsDuplicate(nums []int) bool {
 			return true
 		}
 		hash[v] = i
+	}
+	return false
+}
+
+// For loop(exceeding the time limit)
+// Time  complexity: O(n * n)
+// Space complexity: O(n * n)
+func containsDuplicate1(nums []int) bool {
+	len := len(nums)
+	for i := 0; i < len-1; i++ {
+		for j := i + 1; j < len; j++ {
+			if nums[i] == nums[j] {
+				return true
+			}
+		}
 	}
 	return false
 }
--- contains_duplicate_test.go
+++ contains_duplicate_test.go
@@ -9,3 +9,8 @@ func TestContainsDuplicate(t *testing.T) {
 	nums := []int{1, 2, 3, 4}
 	fmt.Println(containsDuplicate(nums))
 }
+
+func Test1ContainsDuplicate(t *testing.T) {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 7}
+	fmt.Println(containsDuplicate1(nums))
+}
