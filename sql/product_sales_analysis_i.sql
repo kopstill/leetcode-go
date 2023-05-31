fc02d3fdf4328939f1fc242919dcf64387f1495b
kopever <kop.still@gmail.com>
2023-05-31 23:00:57 +0800

    problem1068: left join

---
+++ sql/product_sales_analysis_i.sql
@@ -0,0 +1,8 @@
+/*
+    1068: https://leetcode.cn/problems/product-sales-analysis-i/
+ */
+
+-- Write your MySQL query statement below
+
+-- left join
+select p.product_name, s.year, s.price from sales s left join product p on s.product_id = p.product_id
