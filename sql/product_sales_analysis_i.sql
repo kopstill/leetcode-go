6e7a4a31fd441d191db86818dd03135a733b8d72
kopever <kop.still@gmail.com>
2023-05-31 23:26:37 +0800

    problem1068: inner join

--- sql/product_sales_analysis_i.sql
+++ sql/product_sales_analysis_i.sql
@@ -6,3 +6,6 @@
 
 -- left join
 select p.product_name, s.year, s.price from sales s left join product p on s.product_id = p.product_id
+
+-- inner join
+select p.product_name, s.year, s.price from sales s, product p on s.product_id = p.product_id
