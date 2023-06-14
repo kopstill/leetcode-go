aa6552c36d29e3fc69c604c47c1b6bae5d862824
kopever <kop.still@gmail.com>
2023-06-14 23:37:05 +0800

    problem619: subquery

---
+++ sql/biggest_single_number.sql
@@ -0,0 +1,9 @@
+/*
+  619: https://leetcode.cn/problems/biggest-single-number/
+ */
+
+-- Write your MySQL query statement below
+
+-- subquery
+select (select num from MyNumbers group by num having count(*) = 1 order by num desc limit 1) as num;
+select (select num from MyNumbers group by num having count(*) = 1 order by num desc limit 1) as num from dual;
