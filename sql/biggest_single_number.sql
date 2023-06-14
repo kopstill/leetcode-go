8fd7b99c7ffe82817dcebee4edb6eb4ffadcd90c
kopever <kop.still@gmail.com>
2023-06-14 23:40:52 +0800

    problem619: max function

--- sql/biggest_single_number.sql
+++ sql/biggest_single_number.sql
@@ -7,3 +7,6 @@
 -- subquery
 select (select num from MyNumbers group by num having count(*) = 1 order by num desc limit 1) as num;
 select (select num from MyNumbers group by num having count(*) = 1 order by num desc limit 1) as num from dual;
+
+-- max function
+select max(num) as num from (select num from MyNumbers group by num having count(num) = 1) as t;
