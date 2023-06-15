f9e0407ba42b9aa6329d4fb7679fd13948dd8f58
kopever <kop.still@gmail.com>
2023-06-15 23:04:25 +0800

    problem 1211: with avg function

--- sql/queries-quality-and-percentage.sql
+++ sql/queries-quality-and-percentage.sql
@@ -12,3 +12,22 @@ from
   Queries 
 group by 
   query_name
+
+-- avg
+select 
+  query_name, 
+  round(avg(rating / position), 2) as quality, 
+  round(sum(if(rating < 3, 1, 0)) / count(*) * 100, 2) as poor_query_percentage 
+from 
+  Queries 
+group by 
+  query_name
+
+select 
+  query_name, 
+  round(avg(rating / position), 2) as quality, 
+  round(avg(rating < 3) * 100, 2) as poor_query_percentage 
+from 
+  Queries 
+group by 
+  query_name
