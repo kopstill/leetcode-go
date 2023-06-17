e14daf6b6151aaa3b42a60900365b38373d5b72c
kopever <kop.still@gmail.com>
2023-06-17 13:41:05 +0800

    problem 1251: subquery

--- sql/average_selling_price.sql
+++ sql/average_selling_price.sql
@@ -14,3 +14,18 @@ where
   us.purchase_date between p.start_date and p.end_date 
 group by 
   p.product_id
+
+-- subquery
+SELECT
+    product_id,
+    Round(SUM(sales) / SUM(units), 2) AS average_price
+FROM (
+    SELECT
+        Prices.product_id AS product_id,
+        Prices.price * UnitsSold.units AS sales,
+        UnitsSold.units AS units
+    FROM Prices 
+    JOIN UnitsSold ON Prices.product_id = UnitsSold.product_id
+    WHERE UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
+) T
+GROUP BY product_id
